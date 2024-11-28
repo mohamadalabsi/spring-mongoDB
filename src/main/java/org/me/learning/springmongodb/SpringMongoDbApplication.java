902a27250d845c7c884e4df4f4f727bf265ecd25
mohamadalabsi <absimoda1@gmail.com>
2024-11-28 14:46:23 +0100

    connecting to MongoDB and some issues with enabling swagger

--- src/main/java/org/me/learning/springmongodb/SpringMongoDbApplication.java
+++ src/main/java/org/me/learning/springmongodb/SpringMongoDbApplication.java
@@ -2,12 +2,71 @@ package org.me.learning.springmongodb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SpringMongoDbApplication {
 
+    //        now i want to just to know how to use swagger so i have to copy past some code here
+
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("org.me.learning.springmongodb"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Your API Title")
+                .description("Your API Description")
+                .version("1.0.0")
+                .build();
+    }
+
+
+
+//    public Docket api() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .select()
+//                .apis(RequestHandlerSelectors.any()) // You can filter specific packages here
+//                .paths(PathSelectors.any())         // You can filter specific paths here
+//                .build();
+//    }
+
+
+
+
+
+//
+//    @Bean
+//    public Docket api() {
+//        return new Docket(DocumentationType.SWAGGER_2).select()
+//                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class)).paths(PathSelectors.any())
+//                .build().apiInfo(apiInfo()).useDefaultResponseMessages(false);
+//    }
+//    @Bean
+//    public ApiInfo apiInfo() {
+//        final ApiInfoBuilder builder = new ApiInfoBuilder();
+//        return builder.build();
+//    }
     public static void main(String[] args) {
         SpringApplication.run(SpringMongoDbApplication.class, args);
+
     }
 
 }
